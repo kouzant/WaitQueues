13eb118749836c77c1d6d00c4ae3dae60668b814
alafroiskiotos <praetorian@akyroi.gr>
2011-07-21 14:58:10 +0300

    Basic Flights entity

---
+++ src/entities/Flights.java
@@ -0,0 +1,71 @@
+package entities;
+
+import java.util.Date;
+
+public class Flights {
+    private String flightCode;
+    private String departure;
+    private String arrival;
+    private Date depTime;
+    private Date arrTime;
+
+    public Flights(String flightCode, String departure, String arrival,
+            Date depTime, Date arrTime){
+        this.flightCode = flightCode;
+        this.departure = departure;
+        this.arrival = arrival;
+        this.depTime = depTime;
+        this.arrTime = arrTime;
+    }
+    public String getFlightCode(){
+        return flightCode;
+    }
+    public String getDeparture(){
+        return departure;
+    }
+    public String getArrival(){
+        return arrival;
+    }
+    public Date getDepTime(){
+        return depTime;
+    }
+    public Date getArrTime(){
+        return arrTime;
+    }
+    public void setFlightCode(String flightCode){
+        this.flightCode = flightCode;
+    }
+    public void setDeparture(String departure){
+        this.departure = departure;
+    }
+    public void setArrival(String arrival){
+        this.arrival = arrival;
+    }
+    public void setDepTime(Date depTime){
+        this.depTime = depTime;
+    }
+    public void setArrTime(Date arrTime){
+        this.arrTime = arrTime;
+    }
+    
+    @Override
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("Flight Code: ");
+        sb.append(flightCode);
+        sb.append("\n");
+        sb.append("Departure: ");
+        sb.append(departure);
+        sb.append("\n");
+        sb.append("Arrival: ");
+        sb.append(arrival);
+        sb.append("\n");
+        sb.append("Departure Time: ");
+        sb.append(depTime);
+        sb.append("\n");
+        sb.append("Arrival Time: ");
+        sb.append(arrTime);
+        sb.append("\n");
+        return sb.toString();
+    }
+}
